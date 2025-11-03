8e070164711074da460aca75fd568cd25a0fa243
andras <ruijiang5014@gmail.com>
2025-11-03 06:54:16 +0000

    ws subscribe kline

--- internal/service/exchange/binance/market.go
+++ internal/service/exchange/binance/market.go
@@ -2,6 +2,7 @@ package binance
 
 import (
 	"context"
+	"log"
 	"time"
 
 	"github.com/KNICEX/trading-agent/internal/service/exchange"
@@ -79,8 +80,85 @@ func (m *MarketService) GetKlines(ctx context.Context, req exchange.GetKlinesReq
 }
 
 func (m *MarketService) SubscribeKline(ctx context.Context, tradingPair exchange.TradingPair, interval exchange.Interval) (chan exchange.Kline, error) {
-	// TODO websocket subscribe kline
-	ch := make(chan exchange.Kline)
+	ch := make(chan exchange.Kline, 10)
+
+	// 启动WebSocket订阅
+	doneC, stopC, err := futures.WsKlineServe(
+		tradingPair.ToString(),
+		interval.ToString(),
+		func(event *futures.WsKlineEvent) {
+			// 只处理已关闭的K线
+			if !event.Kline.IsFinal {
+				return
+			}
+
+			// 转换K线数据
+			klineOpen, err := decimal.NewFromString(event.Kline.Open)
+			if err != nil {
+				return
+			}
+			klineClose, err := decimal.NewFromString(event.Kline.Close)
+			if err != nil {
+				return
+			}
+			klineHigh, err := decimal.NewFromString(event.Kline.High)
+			if err != nil {
+				return
+			}
+			klineLow, err := decimal.NewFromString(event.Kline.Low)
+			if err != nil {
+				return
+			}
+			klineVolume, err := decimal.NewFromString(event.Kline.Volume)
+			if err != nil {
+				return
+			}
+			klineQuoteAssetVolume, err := decimal.NewFromString(event.Kline.QuoteVolume)
+			if err != nil {
+				return
+			}
+
+			kline := exchange.Kline{
+				OpenTime:         time.UnixMilli(event.Kline.StartTime),
+				CloseTime:        time.UnixMilli(event.Kline.EndTime),
+				Open:             klineOpen,
+				Close:            klineClose,
+				High:             klineHigh,
+				Low:              klineLow,
+				Volume:           klineVolume,
+				QuoteAssetVolume: klineQuoteAssetVolume,
+			}
+
+			// 发送K线数据到channel
+			select {
+			case ch <- kline:
+			case <-ctx.Done():
+				return
+			}
+		},
+		func(err error) {
+			log.Fatalf("ws kline error: %v", err)
+		},
+	)
+
+	if err != nil {
+		close(ch)
+		return nil, err
+	}
+
+	// 启动协程处理context取消和清理
+	go func() {
+		select {
+		case <-ctx.Done():
+			// context被取消，关闭WebSocket
+			close(stopC)
+			close(ch)
+		case <-doneC:
+			// WebSocket连接断开
+			close(ch)
+		}
+	}()
+
 	return ch, nil
 }
 
